f7cd74c0a429c7870fdd05f43f5ad5f37d82be86
Syawal Jasira <syawaljasira@gmail.com>
2022-07-20 07:44:37 +0700

    fix webkit/safari issue

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,37 +4,6 @@ import "./Footer.scss";
 const Footer = () => {
   return (
     <footer className="footer">
-      <div id="contact" className="footer__gap"></div>
-      <div className="footer__contactUs">
-        <form className="footer__contactUs-card">
-          <h4 className="form-title">Contact Us</h4>
-          <span className="input-group">
-            <label htmlFor="name">Name</label>
-            <input type="text" name="name" id="name" placeholder="Nama" />
-          </span>
-          <span className="input-group">
-            <label htmlFor="email">Email</label>
-            <input
-              type="email"
-              name="email"
-              id="email"
-              placeholder="Email Address"
-            />
-          </span>
-          <span className="input-group">
-            <label htmlFor="message">Message</label>
-            <input
-              type="text"
-              name="message"
-              id="message"
-              placeholder="Message"
-            />
-          </span>
-          <span className="form-btn">
-            <button>Send</button>
-          </span>
-        </form>
-      </div>
       <div className="footer__address">
         <div className="footer__address-item">
           <h5>Our Office</h5>
@@ -50,10 +19,10 @@ const Footer = () => {
             Pasir Sari. Cikarang - Bekasi Indonesia
           </address>
         </div>
-        <p className="footer__copyright">
-          Copyright &copy; 2021 PT. SSI Prima Mas. All Rights Reserved
-        </p>
       </div>
+      <p className="footer__copyright">
+        Copyright &copy; 2021 PT. SSI Prima Mas. All Rights Reserved
+      </p>
     </footer>
   );
 };
